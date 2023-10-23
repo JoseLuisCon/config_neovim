b9b0b34a2e0e6efe62b309218258335d863d108a
JoseLuisCon <condepa45@gmail.com>
2023-10-23 11:13:31 +0200

    Inicital config NEOVIM + CUSTOM Treesitter + Copilot

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,27 @@
+local options = {
+  ensure_installed = { "lua", "luadoc", "json", "yaml", "html", "css", "javascript", "typescript", "tsx", "rust", "go", "java", "c", "cpp", "bash", "vim",  "latex", "markdown", "regex", "comment", "query", "python" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+         -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ic"] = "@conditional.inner",
+        ["ac"] = "@conditional.outer",
+        ["il"] = "@loop.inner",
+        ["al"] = "@loop.outer",
+     }
+    }
+  }
+}
+
+return options
